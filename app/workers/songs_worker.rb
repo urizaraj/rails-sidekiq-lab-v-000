810e6e66d3e1cf8d70095bf080a0456bef51842c
Alexander Urizar <urizaraj@gmail.com>
2018-04-02 17:45:14 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -3,7 +3,7 @@ class SongsWorker
   require 'csv'
 
   def perform(file)
-    CSV.foreach(params["file"].path, headers: true) do |song|
+    CSV.foreach(file, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
     end
   end
